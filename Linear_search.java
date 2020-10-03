4f69b7972487aa48958d00daff1900c54894049e
Drishti Malhotra <71998960+Drishti05@users.noreply.github.com>
2020-10-03 13:23:50 +0530

    Add files via upload

---
+++ Linear_search.java
@@ -0,0 +1,42 @@
+package linear_search;
+
+import java.util.Scanner;
+
+public class Linear_search {
+    public static void main(String[] args) {
+       System.out.println("Enter the number of elements in the array");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); //Taking input for size of the array
+        int found = 0;
+        int arr[] = new int[n]; // Array declaration and initialization
+        System.out.println("Enter the elements");
+        for(int i=0;i<n;i++) // for loop to take the array elements as input
+        {
+             arr[i] = sc.nextInt();
+            
+        }
+        System.out.println("Enter the element to be searched");
+        int num = sc.nextInt(); // Taking input of the number to be searched
+        for(int j=0;j<n;j++) // for loop to iterate over the array
+        {
+           if(num == arr[j]) // condition to check the presence of the element
+           {
+               found=1;
+               break;
+           }
+               
+        
+        }
+        if(found ==1) // if element is there in the list
+        {
+            System.out.println("The number "+num+ " is there in the list");
+        
+        }  
+        else // if element is not there in the list
+        {
+            System.out.println("The number "+num+ " is not there in the list");
+        }    
+            
+    }
+    
+}
