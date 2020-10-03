6de40321b3216dd67f5888027a0a7af442101a00
Drishti Malhotra <71998960+Drishti05@users.noreply.github.com>
2020-10-03 13:35:28 +0530

    Update Linear_search.java

--- Linear_search.java
+++ Linear_search.java
@@ -8,6 +8,7 @@ public class Linear_search {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt(); //Taking input for size of the array
         int found = 0;
+        int pos = -1; // to store the location at which number is found in the array 
         int arr[] = new int[n]; // Array declaration and initialization
         System.out.println("Enter the elements");
         for(int i=0;i<n;i++) // for loop to take the array elements as input
@@ -21,7 +22,8 @@ public class Linear_search {
         {
            if(num == arr[j]) // condition to check the presence of the element
            {
-               found=1;
+               found = 1;
+               pos = j+1;
                break;
            }
                
@@ -29,12 +31,12 @@ public class Linear_search {
         }
         if(found ==1) // if element is there in the list
         {
-            System.out.println("The number "+num+ " is there in the list");
+            System.out.println("The number "+num+ " is there in the array at position "+ pos );
         
         }  
         else // if element is not there in the list
         {
-            System.out.println("The number "+num+ " is not there in the list");
+            System.out.println("The number "+num+ " is not there in the array");
         }    
             
     }
